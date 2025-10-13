d96d0d5827ffd7c12b84a59bac8e1b9b69d4a3a2
Jan Gumbau <jan.gumbau@enti.cat>
2025-10-13 23:02:46 +0200

    Finished Character Controller

--- Assets/Projecte/Scripts/Character/Character_Controller.cs
+++ Assets/Projecte/Scripts/Character/Character_Controller.cs
@@ -2,26 +2,65 @@ using UnityEngine;
 
 public class Character_Controller : MonoBehaviour
 {
+    //contar Carrils  i definir velocitat
+    private int laneCount = 3;
+    private float laneSpacing = 4f;
+    public float coyoteTime = 0.25f;
+
     
-     private float step = 2f;
+    
+
+    private float centerX;
+    private int currentLane;
+    private float targetX;
+    private float coyote;
+
     void Start()
     {
-        
+        centerX = transform.position.x;
+        currentLane = laneCount / 2; //3 / 2 = 1 -> carril central
+        Snap();
+
     }
 
     // Update is called once per frame
 
-   private  void FixedUpdate()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * step;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * step;
-        }
-     
-            
+        if(coyote > 0f) coyote -= Time.deltaTime; 
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            ChangeLane(-1);
+
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            ChangeLane(1);
+        
+        
+           
+        
+
     }
+
+    private void ChangeLane(int direction)
+    {
+        if(coyote > 0f) return;
+        int newLane = Mathf.Clamp(currentLane + direction, 0, laneCount - 1);
+        if (newLane == currentLane) return;
+
+        currentLane = newLane;
+        Snap();
+        coyote = coyoteTime;
+
+        
+        
+            
+        
+    }
+    private void Snap()
+    {
+        
+        float x = centerX + ((currentLane - (laneCount - 1) * 0.5f) * laneSpacing);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+
 }
